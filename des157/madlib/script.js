b53bb6e559926cd9ac63061d91207eaac8756b5d
Erika Lama} <erikalama@erikas-mbp.attlocal.net>
2021-02-02 12:55:00 -0800

    Added BG imh

--- des157/madlib/script.js
+++ des157/madlib/script.js
@@ -1,6 +1,8 @@
 (function(){
     'use strict';
 
+    // document.getElementById("output").style.visibility = "hidden";
+
     const myForm = document.querySelector('#myForm');
     const madlibs = document.querySelector('#output');
 
@@ -14,13 +16,13 @@
 
     
 
-        ////// will add the story here (output)//////
+        ////// will add the story here (in the if statement)//////
 
         
         var myText ;
         var story 
         if (name && animal && verb && cartoon && adjective) {
-             myText = `Here are the words: ${name}, ${animal}, ${verb}, ${cartoon}, ${adjective}`;
+            //  myText = `Here are the words: ${name}, ${animal}, ${verb}, ${cartoon}, ${adjective}`;
               story = "</br>Hello <strong>" + name + "</strong>, I know you love to sing, but why do you sound like <strong>" + animal +"</strong> Why don't you <strong>" + verb + "</strong> and sing with me like <strong>" + cartoon+ "</strong>, I bet you like that better. Okay okay, now let's keep <strong>"+ adjective + "</strong> until we get to the right note. Let's hit it up to D#!" ;
 
         }
@@ -29,9 +31,11 @@
         }
         
 
-        madlibs.innerHTML = myText
+        // madlibs.innerHTML = myText
         
           madlibs.innerHTML += story
+
+        // document.getElementById("output").style.visibility = "visible";
 
     });
 
